91c58021912e56d4fda516c2dbc84512401e26f8
safroniv <safroniv@mail.ru>
2022-06-21 15:19:46 +0300

    GB Урок 4 - занятие

---
+++ Incapsilation.Data/Specifications/CustomerByAgeSpecification.cs
@@ -0,0 +1,25 @@
+﻿namespace Incapsilation.Data.Specifications
+{
+    public class CustomerByAgeSpecification : SpecificationBase
+    {
+        private int _age;
+
+        public CustomerByAgeSpecification(int specAge)
+        {
+            _age = specAge;
+            _test = 115;
+        }
+        //override это реализация абстрактного метода в наследуемом классе
+        //если добавить sealed - то мы запечатаем метод от наследования в дальнейшем
+        protected sealed override bool CheckSpecification(Customer customer)
+        {
+            //return true; // это просто пока заглушка
+            return customer.Age == _age;
+        }
+
+        public override void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- JobLessonOOP08v03Part01/Program.cs
+++ JobLessonOOP08v03Part01/Program.cs
@@ -1,1 +1,37 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Incapsilation.Data;
+using Incapsilation.Data.Specifications;
+
+var storage = new InMemoryCustomerDataStorage();
+var newCastomer = storage.CreateCustomer(1, "Ivan", 37);
+
+var cust = storage.GetById(10);
+if (cust != null)
+{
+    cust.Name = "new name";
+    _ = storage.UpdateCustomer(cust.Id, cust);
+
+}
+Console.WriteLine(newCastomer.Id + newCastomer.Name + newCastomer.Age);
+//var age10Spec = new CustomerByAgeSpecification(10);
+//var age10Customers = storage.GetBySpecification(age10Spec);
+
+//var age37Spec = new CustomerByAgeSpecification(37);
+//var age37Customers = storage.GetBySpecification(age37Spec);
+//Console.WriteLine(age10Customers.Length);
+//Console.WriteLine(age37Customers.Length);
+
+//var abstractSpec = new SpecificationBase();
+//при попытке создания экземпляра абстрактного класса - 
+//не получается создать экземпляр абстрактного класса, ругается компилятор!
+//у абстрактного класса нет instance (реализации).
+
+//Чаще всего вместо абстрактных классов создаются интерфейсы.
+
+//замена public Customer [] GetBySpecification (CustomerByAgeSpecification spec) 
+//    на public Customer[] GetBySpecification(SpecificationBase spec)
+// позволяет применять спеки разных типов
+
+var nameSpec = new CustomerByNameSpecification("ivaN");
+var customerByName = storage.GetBySpecification(nameSpec);
+
+Console.WriteLine(customerByName.Length);
